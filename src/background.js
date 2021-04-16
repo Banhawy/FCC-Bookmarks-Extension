a05c78c09243022151b9944cdb2a2b6a71f60cb8
Adham El Banhawy <the.benhawy@gmail.com>
2021-04-17 00:37:07 +0200

    moved to if block to avoid duplication

--- src/background.js
+++ src/background.js
@@ -12,28 +12,29 @@ runtime.onInstalled.addListener((details) => {
                 console.log(`Created bookmark`, bookmark)
                 FCCFolder = bookmark
             })
+
+						// Look for any previously bookmarked FCC articles and copy them to the FCC Articles Folder
+						bookmarks.search('https://freecodecamp.org/news/', (searchedBookmarksTree) => {
+							if (searchedBookmarksTree.length > 0) {
+								// Create an array of just bookmarked url strings
+								let FCCBookmarks = searchedBookmarksTree.map(bookmark => bookmark.url)
+								// Check if the root news route is present in the bookmarks & remove it
+								const rootNewsSiteIndex = FCCBookmarks.indexOf('https://www.freecodecamp.org/news/')
+								const rootNewsSiteIsPresent = rootNewsSiteIndex > -1
+								if (rootNewsSiteIsPresent) {
+									searchedBookmarksTree.splice(rootNewsSiteIndex ,1)
+								}
+								// Map through each bookemarked FCC Article and copy it to our extension's bookmarks folder
+								searchedBookmarksTree.forEach((fccArticle) => {
+									bookmarks.create({
+											parentId: FCCFolder.id,
+											title: fccArticle.title,
+											url: fccArticle.url
+									})
+								})
+							}
+						})
         }
 
-				// Look for any previously bookmarked FCC articles and copy them to the FCC Articles Folder
-				bookmarks.search('https://freecodecamp.org/news/', (searchedBookmarksTree) => {
-					if (searchedBookmarksTree.length > 0) {
-						// Create an array of just bookmarked url strings
-						let FCCBookmarks = searchedBookmarksTree.map(bookmark => bookmark.url)
-						// Check if the root news route is present in the bookmarks & remove it
-						const rootNewsSiteIndex = FCCBookmarks.indexOf('https://www.freecodecamp.org/news/')
-						const rootNewsSiteIsPresent = rootNewsSiteIndex > -1
-						if (rootNewsSiteIsPresent) {
-							searchedBookmarksTree.splice(rootNewsSiteIndex ,1)
-						}
-						// Map through each bookemarked FCC Article and copy it to our extension's bookmarks folder
-						searchedBookmarksTree.forEach((fccArticle) => {
-							bookmarks.create({
-									parentId: FCCFolder.id,
-									title: fccArticle.title,
-									url: fccArticle.url
-							})
-						})
-					}
-				})
     })
 })
