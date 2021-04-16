bcc903e84b91fe215e0c82e7a81c4b0105a786c2
Adham El Banhawy <the.benhawy@gmail.com>
2021-04-16 20:29:28 +0200

    Create FCC folder & move existing articles to it

--- src/background.js
+++ src/background.js
@@ -1,1 +1,30 @@
-import "crx-hotreload";
+const { runtime, bookmarks } = chrome
+runtime.onInstalled.addListener((details) => {
+    bookmarks.getTree((bookmarksTree) => {
+				// Find an existing 'FCC Articles' bookmarks folder
+        let FCCFolder = bookmarksTree[0].children[1].children.filter(
+            (child) => child.title === 'FCC Articles'
+        )[0]
+				// If it doesn't exist create it
+        if (!FCCFolder) {
+            bookmarks.create({ title: 'FCC Articles' }, (bookmark) => {
+                console.log(`Created bookmark`, bookmark)
+                FCCFolder = bookmark
+            })
+        }
+        // Look for any previously bookmarked FCC articles and copy them to the FCC Articles Folder
+        bookmarks.search('https://freecodecamp.org', (bookmarksTree) => {
+            if (bookmarksTree.length > 0) {
+                bookmarks.get(FCCFolder.id, (folder) => {
+                    bookmarksTree.forEach((fccArticle) => {
+                        bookmarks.create({
+                            parentId: FCCFolder.id,
+                            title: fccArticle.title,
+                            url: fccArticle.url
+                        })
+                    })
+                })
+            }
+        })
+    })
+})
