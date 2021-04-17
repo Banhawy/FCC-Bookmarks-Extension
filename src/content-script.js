c10ea23a36be7ebf1acc637c44eea2674e20c2d9
Adham El Banhawy <the.benhawy@gmail.com>
2021-04-17 03:53:41 +0200

    renamed variable

--- src/content-script.js
+++ src/content-script.js
@@ -6,7 +6,7 @@ const injectBookmarksButton = articleInBookmarks => {
     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 4v12l-4-2-4 2V4M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" /></svg>`
     const removeIcon = `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>`
-    const tail = `
+    const buttonHTML = `
     <style>${cssText}</style>
     <div class="mt-5 flex lg:mt-0 lg:ml-4">
         <span class="hidden sm:block">
@@ -27,7 +27,7 @@ const injectBookmarksButton = articleInBookmarks => {
         authorDiv.insertAdjacentElement('beforeend', bookmarkDiv)
         // Create a shadow DOM to isolate our custom div from external styles and apply only our own
         const shadowRoot = bookmarkDiv.attachShadow({ mode: 'open' })
-        shadowRoot.innerHTML = tail
+        shadowRoot.innerHTML = buttonHTML
     }
 }
 
