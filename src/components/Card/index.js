3520daf81143ba25061ea951dc4a96f481a82964
Adham El Banhawy <the.benhawy@gmail.com>
2021-04-20 20:58:39 +0200

    added bookmark info & opened in new tab

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
-const Card = () => {
+const Card = ({ bookmark }) => {
+    const handleClick = (_) => chrome.tabs.create({ url: bookmark.url })
     return (
-        <div className="py-4 flex rounded bg-white mt-4 mb-8 mx-1 p-4">
-            <h2 className="text-2xl font-bold text-center">Hi I'm a FREAKIN CARD</h2>
+        <div
+            className="py-4 flex rounded bg-white mt-4 mb-8 mx-1 p-4"
+            onClick={handleClick}
+        >
+            <h2 className="text-xs font-bold text-center">{bookmark.title}</h2>
         </div>
     )
 }
